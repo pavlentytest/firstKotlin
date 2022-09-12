4e41cc930a2a9ad417df6d9f875a89920ed06f09
Pavel <impetus@yandex.ru>
2022-09-12 18:53:35 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+import java.net.URL
+import java.util.*
+
+fun main() {
+    println("Hello World!")
+   // val address = "https://ya.ru"
+  //  val someText = URL(address).readText()
+  //  println(someText);
+
+/*
+Блочный комментарий
+ */
+
+
+    var a1: String = "123"
+
+    var a0: Short = 11 // 16
+
+    var a2: Int = 123 // 32
+    var a3: Long = 123 // 64
+
+    var a4: Double = 1.45  // 64
+    var a5: Float = 1.45F  // 32
+    var c1: Char = 'A'
+
+    var b1: Long = a2.toLong()
+    var flag: Boolean = true
+    println(sqr2(5))
+
+    /*
+    val bb: Scanner = Scanner(System.`in`)
+    val aa = bb.nextInt()
+    println(aa)
+    */
+
+    val y = readLine()?.toInt()
+    println(y)
+
+}
+fun sqr(x: Int): Int {
+    return x*x
+}
+fun sqr2(x:Int) = x*x
